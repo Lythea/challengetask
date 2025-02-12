2f498de0959ac0a212d39b580beb4a343d4abfa7
ron <infinitech.roniversondelmundo@gmail.com>
2025-02-12 13:10:35 +0800

    reverseString

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Hello, World!";
+        string reversed = ReverseString(input);
+        Console.WriteLine(input); //Original String
+        Console.WriteLine(reversed); //Reverse String
+    }
+
+    static string ReverseString(string s)
+    {
+        char[] arr = s.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
+    }
+}
